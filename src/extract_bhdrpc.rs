341e7fc0063f38043266b24a5bf614c4474ed8d3
JohnnyFFM <JohnnyDeluxe@habmalnefrage.de>
2019-06-27 19:22:27 +0200

    csv extract done

--- src/extract_bhdrpc.rs
+++ src/extract_bhdrpc.rs
@@ -1,5 +1,54 @@
 extern crate jsonrpc;
 extern crate serde;
+use pbr::ProgressBar;
+
+#[serde(default)]
+#[derive(Serialize, Deserialize, Default)]
+pub struct Block {
+    #[serde(skip_serializing)]
+    hash: String,
+    #[serde(skip_serializing)]
+    confirmations: u64,
+    #[serde(skip_serializing)]
+    #[serde(rename = "strippedsize")]
+    stripped_size: u64,
+    #[serde(skip_serializing)]
+    size: u64,
+    #[serde(skip_serializing)]
+    weight: u64,
+    height: u64,
+    #[serde(skip_serializing)]
+    version: u64,
+    #[serde(rename = "versionHex")]
+    #[serde(skip_serializing)]
+    version_hex: String,
+    #[serde(skip_serializing)]
+    #[serde(rename = "merkleroot")]
+    merkle_root: String,
+    #[serde(skip_serializing)]
+    tx: Vec<String>,
+    time: u64,
+    #[serde(skip_serializing)]
+    #[serde(rename = "mediantime")]
+    median_time: u64,
+    #[serde(skip_serializing)]
+    difficulty: f64,
+    #[serde(skip_serializing)]
+    chainwork: String,
+    #[serde(rename = "baseTarget")]
+    base_target: u64,
+    #[serde(rename = "plotterId")]
+    plotter_id: u64,
+    nonce: u64,
+    #[serde(rename = "generationSignature")]
+    generation_signature: String, 
+    deadline: u64,
+    generator: String, 
+    #[serde(skip_serializing)]
+    previousblockhash: String,
+    #[serde(skip_serializing)]
+    nextblockhash: String,        
+}
 
 pub fn extract_bhdrpc(matches: &clap::ArgMatches) {
     // read cookie
@@ -17,6 +66,7 @@ pub fn extract_bhdrpc(matches: &clap::ArgMatches) {
     let client =
         jsonrpc::client::Client::new(wallet, Some("__cookie__".to_owned()), Some(pass.to_owned()));
     let request = client.build_request("getblockcount", &[]);
+    // query block count
     let blockcount = match client
         .send_request(&request)
         .and_then(|res| res.into_result::<u64>())
@@ -27,11 +77,20 @@ pub fn extract_bhdrpc(matches: &clap::ArgMatches) {
             0u64
         }
     };
-    
-    // query block count
     println!("Blockcount : {}", blockcount);
+
+    // initialise write  
+    let filename = format!("bhd_{}.csv",blockcount);
+    println!("Filename   : {}", &filename);
+    let mut wtr = csv::Writer::from_path(&filename).unwrap();
+
+    let mut pb = ProgressBar::new(blockcount);
+    pb.format("│██░│");
+    pb.set_width(Some(80));
+    pb.message("Extracting: ");
+
     // extract blocks
-    for i in 0..10 as u64 {
+    for i in 0..blockcount as u64 {
         let param = vec![serde_json::to_value(i).unwrap()];
         let request = client.build_request("getblockhash", &param);
         let blockhash = match client
@@ -44,7 +103,21 @@ pub fn extract_bhdrpc(matches: &clap::ArgMatches) {
                 "".to_owned()
             }
         };
-        println!("BlockHash   : {}", blockhash);
+        let param = vec![serde_json::to_value(blockhash).unwrap()];
+        let request = client.build_request("getblock", &param);
+        let block = match client
+            .send_request(&request)
+            .and_then(|res| res.into_result::<Block>())
+        {
+            Ok(res) => res, // Ok!
+            Err(e) => {
+                println!("{:?}", e);
+                Block::default()
+            }
+        };
+        // export to csv
+        wtr.serialize(block).expect("error deserialising block info");
+        pb.inc();
     }
-    // export to csv
+    pb.finish_print("done.");
 }
