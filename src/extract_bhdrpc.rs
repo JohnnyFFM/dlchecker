ff2e556436254503f29fd09173e2c16ce8119ee7
JohnnyFFM <jonas.steitz@gmx.net>
2019-06-27 11:42:21 +0200

    cookie auth

--- src/extract_bhdrpc.rs
+++ src/extract_bhdrpc.rs
@@ -4,9 +4,14 @@ extern crate serde;
 pub fn extract_bhdrpc(matches: &clap::ArgMatches){
     // read cookie
     let cookiepath = value_t!(matches, "cookie", String).unwrap_or_else(|e| e.exit());
+    let cookiepath = std::path::Path::new(&cookiepath);
+    let cookiepath = cookiepath.join(".cookie");
+    println!("Cookie Path: {:?}", cookiepath);
+    let cookie: String = std::fs::read_to_string(cookiepath).expect("can't read cookie file").parse().expect("can't parse cookie file");
+    let pass = &cookie[11..];
     // start rpc
     let wallet = value_t!(matches, "wallet", String).unwrap_or_else(|e| e.exit());
-    let client = jsonrpc::client::Client::new(wallet, Some("__cookie__".to_owned()), Some("67d4c4f38d1911cac58df0d1b714db0af9912c4536336f4ecd193c652a674844".to_owned()));
+    let client = jsonrpc::client::Client::new(wallet, Some("__cookie__".to_owned()), Some(pass.to_owned()));
     let request = client.build_request("getblockcount", &[]);
     let blockcount = match client.send_request(&request).and_then(|res| res.into_result::<u64>()) {
         Ok(res) => res, // Ok!
@@ -15,8 +20,8 @@ pub fn extract_bhdrpc(matches: &clap::ArgMatches){
             0u64
         }
     };
+    // query block count
     println!("Blockcount : {}", blockcount);
-    // query block height
     // extract blocks
     // export to csv
 }
